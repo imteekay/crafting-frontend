16ea9cf7ef2c9c7784a9c9ed840ded7bfb8b7738
TK <leandrotk100@gmail.com>
2022-07-02 20:22:26 -0300

    improvements

--- javascript/memoization/benchmark/index.js
+++ javascript/memoization/benchmark/index.js
@@ -1,30 +1,14 @@
 import { memoizedSum } from './sum.js';
-import { test, getNumbers } from './test.js';
+import { testSum, getNumbers, start, end } from './test.js';
 
 const numbers = getNumbers();
 
-console.log('-------- sum --------');
+start('sum');
+testSum('cold', numbers, memoizedSum);
+testSum('cached', numbers, memoizedSum);
+end();
 
-console.time('cold');
-test(numbers, memoizedSum);
-console.timeEnd('cold');
-
-console.time('cached');
-test(numbers, memoizedSum);
-console.timeEnd('cached');
-
-console.log('-------- // --------');
-console.log();
-
-console.log('-------- sum --------');
-
-console.time('cold');
-test(numbers, memoizedSum);
-console.timeEnd('cold');
-
-console.time('cached');
-test(numbers, memoizedSum);
-console.timeEnd('cached');
-
-console.log('-------- // --------');
-console.log();
+start('sum');
+testSum('cold', numbers, memoizedSum);
+testSum('cached', numbers, memoizedSum);
+end();
--- javascript/memoization/benchmark/test.js
+++ javascript/memoization/benchmark/test.js
@@ -8,8 +8,16 @@ export function getNumbers() {
   return numbers;
 }
 
-export function test(numbers, callback) {
+export function testSum(label, numbers, callback) {
+  console.time(label);
   for (let index = 0; index < numbers.length; index++) {
     callback(numbers[index], numbers[index]);
   }
+  console.timeEnd(label);
 }
+
+export const start = (label) => console.log(`-------- ${label} --------`);
+export const end = () => {
+  console.log('-------- // --------');
+  console.log();
+};
